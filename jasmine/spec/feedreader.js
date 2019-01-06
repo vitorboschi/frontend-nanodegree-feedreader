615be23a830ba429f318f4e1b125eaf8c41a5e7e
Vitor Boschi <vitorboschi@gmail.com>
2019-01-06 10:15:07 -0200

    implement required tests for feed reader

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -27,33 +27,49 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
+        /* Loop through each feed in the allFeeds object and
+         * ensures it has a URL defined and that the URL is
+         * not empty.
          */
+        it('have a url', function() {
+            for (let entry of allFeeds) {
+                expect(entry.url).toBeDefined();
+                expect(entry.url.length).not.toBe(0);
+            }
+        });
 
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
+        /* Loop through all entries in the allFeeds object and
+         * ensures it has a name defined and that the name is
+         * not empty.
          */
+        it('have a name', function() {
+            for (let entry of allFeeds) {
+                expect(entry.name).toBeDefined();
+                expect(entry.name.length).not.toBe(0);
+            }
+        });
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
-
+    /* Test suite for the slide menu*/
+	describe('The menu', function() {
+	
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
-
+		it('starts hidden', function() {
+			expect($('body').hasClass('menu-hidden')).toBe(true);
+		});
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
 
+	});
     /* TODO: Write a new test suite named "Initial Entries" */
 
         /* TODO: Write a test that ensures when the loadFeed
